6164ed158cc6cc5d47f42b69c4bf5983800ad051
ragingwind <ragingwind@gmail.com>
2015-09-12 07:49:45 +0900

    Rearrange task list

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -268,8 +268,13 @@ gulp.task('default', ['clean'], function (cb) {
   runSequence(
     ['copy', 'styles'],
     'elements',
-    ['jshint', 'images', 'fonts', 'html'],
-    'vulcanize','rename-index', // 'cache-config',
+    [
+      'jshint', 
+      'images', 
+      'fonts', 
+      'html'
+    ],
+    'vulcanize','rename-index',
     cb);
 });
 
